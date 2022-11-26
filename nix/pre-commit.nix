ee18ef55280a5a044db1c00ae02ec9a3bd06e274
baduhai <baduhai@pm.me>
2022-11-26 12:48:55 -0300

    work around upstream bug

--- nix/pre-commit.nix
+++ nix/pre-commit.nix
@@ -1,17 +1,19 @@
-let 
-  pkgs = import (
-    fetchTarball "https://github.com/NixOS/nixpkgs/archive/69335c46c48a73f291d5c6f332fb9fe8b8e22b30.tar.gz"
-  ) {};
+let
+  pkgs = import
+    (
+      fetchTarball "https://github.com/NixOS/nixpkgs/archive/69335c46c48a73f291d5c6f332fb9fe8b8e22b30.tar.gz"
+    )
+    { };
   nix-pre-commit-hooks = import (
-    builtins.fetchTarball "https://github.com/cachix/pre-commit-hooks.nix/tarball/master"
+    fetchTarball "https://github.com/cachix/pre-commit-hooks.nix/archive/ebcbfe09d2bd6d15f68de3a0ebb1e4dcb5cd324b.tar.gz"
   );
 
 in
 {
   pre-commit-check = nix-pre-commit-hooks.run {
-     src = ../.;
-     
-     hooks = {
+    src = ../.;
+
+    hooks = {
       prettier.enable = true;
       statix.enable = true;
       black.enable = true;
@@ -20,52 +22,52 @@ in
       trailing-whitespace = {
         enable = true;
         entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/trailing-whitespace-fixer";
-        types = ["text"];
+        types = [ "text" ];
       };
       end-of-file-fixer = {
         enable = true;
         entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/end-of-file-fixer";
-        types = ["text"];
+        types = [ "text" ];
       };
       check-added-large-files = {
         enable = true;
         entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/check-added-large-files";
-        types = ["text"];
+        types = [ "text" ];
       };
       check-merge-conflict = {
         enable = true;
         entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/check-merge-conflict";
-        types = ["text"];
+        types = [ "text" ];
       };
       mixed-line-ending = {
         enable = true;
         entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/mixed-line-ending";
-        types = ["text"];
+        types = [ "text" ];
       };
       check-yaml = {
         enable = true;
         entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/check-yaml";
-        types = ["text"];
+        types = [ "text" ];
         files = "\\.yaml$";
       };
       check-xml = {
         enable = true;
         entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/check-xml";
-        types = ["text"];
+        types = [ "text" ];
         files = "\\.xml$";
-        };
+      };
       check-json = {
         enable = true;
         entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/check-json";
-        types = ["text"];
+        types = [ "text" ];
         files = "\\.json$";
       };
       protolint = {
         enable = true;
         entry = "${pkgs.protolint}/bin/protolint lint -fix";
-        types = ["text"];
+        types = [ "text" ];
         files = "\\.proto$";
       };
-     };
+    };
   };
 }
