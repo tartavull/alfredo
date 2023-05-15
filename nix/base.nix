16885fec2f6f3b63a0765cb83a55169bf0ea0af6
tartavull <ignacio@tinymile.ai>
2023-05-14 20:55:46 -0400

    make it such that the base image includes the repository

--- nix/base.nix
+++ nix/base.nix
@@ -1,16 +1,22 @@
 { config, pkgs, lib, ... }:
 
 {
+  imports = [
+    "${builtins.fetchTarball {
+      url = "https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz";
+      sha256 = "1cp2rpprcfl4mjsrsrpfg6278nf05a0mpl3m0snksvdalfmc5si5";
+    }}/nixos"
+  ];
 
   # Generic settings
   system.stateVersion = "22.11";
 
   # NVIDIA configuration
-  nixpkgs.config =
-    {
-      allowUnfree = true;
-      cudaSupport = true;
-    };
+  nixpkgs.config = {
+    allowUnfree = true;
+    cudaSupport = true;
+  };
+
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -27,26 +33,43 @@
     };
   };
 
+
   # Packages
   environment.systemPackages = with pkgs; [
     git
     vim
+    cachix
   ];
 
-  # Service to always fetch latest changes from master in github:tartavull/alfredo
-  systemd.user.services.geneticFetcher = {
-    enable = true;
-    path = with pkgs; [ git ];
-    after = [ "NetworkManager-wait-online.service" ];
-    serviceConfig = {
-      WorkingDirectory = "~";
-      ExecStart = ''
-        if [ -z "$(ls -A $(pwd)/alfredo)" ]; then
-          git clone https://github.com/tartavull/alfredo.git
-        else
-          git pull main
-        fi
-      '';
-    };
+  nix.settings = {
+    substituters = [
+      "https://tartavull.cachix.org"
+    ];
+    trusted-public-keys = [
+      "tartavull.cachix.org-1:xxmUheA3nzwan59bFhfKEShnPeDXMeii+sWHnbq8PsQ="
+    ];
   };
+
+  # Copy the local folder using home-manager
+  home-manager.users.dev = {
+    home.stateVersion = "22.11"; # add this line
+    /*
+      home.file.alfredo = {
+      source = "${builtins.getEnv "PWD"}/../.";
+      recursive = true;
+      };
+    */
+  };
+  /*
+
+    # Systemd service to build the devShell
+    systemd.user.services.buildDevShell = {
+    enable = true;
+    after = [ "home-manager.service" ];
+    serviceConfig = {
+      WorkingDirectory = "/home/username/foldername";
+      ExecStart = "${pkgs.nix}/bin/nix develop";
+    };
+    };
+  */
 }
