2b9d183a672a9864dc6c1a52e6134a938905bbd5
tartavull <ignacio@tinymile.ai>
2023-05-16 22:49:11 -0400

    add cuda mantainers

--- nix/deployer/base.nix
+++ nix/deployer/base.nix
@@ -11,16 +11,22 @@
   hardware.opengl.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  nix.nixConfig = {
+    extra-substituters = [
+      "https://cuda-maintainers.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
+  };
 
   # Users
   users.users = {
     dev = {
       isNormalUser = true;
       extraGroups = [ "wheel" ];
-      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHD6vu2OC+lMAF6mA+Nbn3aCH8/r9EHkNk8OM5QsV6Iw tartavull@gmail.com" ]; # Don't forget to add your ssh public key here!
     };
     root = {
-      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHD6vu2OC+lMAF6mA+Nbn3aCH8/r9EHkNk8OM5QsV6Iw tartavull@gmail.com" ]; # Don't forget to add your ssh public key here!
       hashedPassword = "!";
     };
   };
